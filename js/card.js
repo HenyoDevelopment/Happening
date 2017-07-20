aedef6d2c0e3a88ad8e895c26994de7e0df933ac
sanidjaya <sanidjaya@gmail.com>
2017-07-20 12:23:02 -0400

    drop down interest options

--- js/card.js
+++ js/card.js
@@ -1,5 +1,17 @@
+// $(document).ready(function() {
+//     $(".interest-btn").on('click',function(){
+//         $(this).children('.icon-star, .icon-star-grey').toggleClass("icon-star icon-star-grey");
+//     });
+// });
+
 $(document).ready(function() {
-    $(".interest-btn").on('click',function(){
-        $(this).children('.icon-star, .icon-star-grey').toggleClass("icon-star icon-star-grey");
+    $(".not-interested").click(function() {
+        $("#event-interest").attr('class', 'card-icon icon-star-grey');
+    });
+    $(".interested").click(function() {
+        $("#event-interest").attr('class', 'card-icon icon-star');
+    });
+    $(".going").click(function() {
+        $("#event-interest").attr('class', 'card-icon icon-checkmark');
     });
 });
