9118d969222e9db8145307738adddb5e28b5fb03
gvallang <gvallang@terpmail.umd.edu>
2017-08-11 12:05:58 -0400

    host validation

---
+++ open-sessions.php
@@ -0,0 +1,16 @@
+<?php
+
+	$_SESSION['edit_event'] = false;
+
+	$_SESSION['image_location'] = "";		
+	$_SESSION['image_name'] = "";
+	$_SESSION['event_name'] = "";
+	$_SESSION['location'] = "";
+	$_SESSION['start_date'] = "";
+	$_SESSION['start_time'] = "";
+	$_SESSION['end_date'] = "";
+	$_SESSION['end_time'] = "";
+	$_SESSION['size'] = "";
+	$_SESSION['description'] = "";
+	$_SESSION['tags'] = "";
+?>
