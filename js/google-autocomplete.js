373af94aece3f10e5cc7bedafadd7d4072343cf6
sanidjaya <sanidjaya@gmail.com>
2017-08-22 11:39:39 -0400

    clean up google-autocomplete.js

--- js/google-autocomplete.js
+++ js/google-autocomplete.js
@@ -1,9 +1,7 @@
 function initAutocomplete() {
     // Create the autocomplete object, restricting the search to geographical
     // location types.
-    autocomplete = new google.maps.places.Autocomplete(
-        /** @type {!HTMLInputElement} */(document.getElementById('location')),
-        {types: ['geocode']});
+    autocomplete = new google.maps.places.Autocomplete(document.getElementById('location'));
 
     // When the user selects an address from the dropdown, populate the address
     // fields in the form.
