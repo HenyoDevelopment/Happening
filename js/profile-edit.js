bc5018dce4cd5726a8befdb97f4e357b74bbc7f7
sanidjaya <sanidjaya@gmail.com>
2017-08-22 22:45:25 -0400

    live editing for profile page

---
+++ js/profile-edit.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	$(".edit-profile-btn").on("click", function(e) {
+        if ($(".save-profile-btn")[0]){
+            saveProfile();
+        } else {
+            //edit name
+            var name_data = $(".fullname").children("h3");
+            var name_id = name_data.attr("id");
+            var new_name_id = name_id+"-form";
+            var curr_name_val = name_data.text();
+            name_data.empty();
+
+            //edit data
+            var bio_data = $(".bio").children("p");
+            var bio_id = bio_data.attr("id");
+            var new_bio_id = bio_id+"-form";
+            var curr_bio_val = bio_data.text();
+            bio_data.empty();
+            
+            // send input to DOM 
+            $('<input type="text" name="'+new_name_id+'" id="'+new_name_id+'" value="'+curr_name_val+'" class="live-edit">').appendTo(name_data);
+            $('<input type="text" name="'+new_bio_id+'" id="'+new_bio_id+'" value="'+curr_bio_val+'" class="live-edit">').appendTo(bio_data);
+            
+            //change btn to save profile
+            $(this).attr('class', 'save-profile-btn');
+        }
+    });
+    // save profile changes function
+	function saveProfile() {
+        // save name
+		var name_data = $(".fullname").children("h3");
+		var new_name_id   = name_data.attr("id");
+		var c_name_input  = "#"+new_name_id+"-form";
+		var e_name_input  = $(c_name_input);
+		var new_name_val  = e_name_input.val();
+		e_name_input.remove();
+        name_data.html(new_name_val);
+        
+        // save description
+        var bio_data = $(".bio").children("p");
+		var new_bio_id   = bio_data.attr("id");
+		var c_bio_input  = "#"+new_bio_id+"-form";
+		var e_bio_input  = $(c_bio_input);
+		var new_bio_val  = e_bio_input.val();
+		e_bio_input.remove();
+		bio_data.html(new_bio_val);
+		
+		$(".save-profile-btn").attr('class', 'edit-profile-btn');
+	};
+});
