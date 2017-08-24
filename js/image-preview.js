8f64ae8ce7785ca81c3cc98eaf115c6acca15084
sanidjaya <sanidjaya@gmail.com>
2017-08-24 14:38:13 -0400

    party parrot galore round 2

--- js/image-preview.js
+++ js/image-preview.js
@@ -3,39 +3,8 @@ $(document).ready(function() {
         image_preview(event);
     });
 
+    // render random party parrot
     $(".event-img-preview").attr("src", ("img/party-parrot/" + party_parrot()));
-
-     // party parrot ftw
-    function party_parrot() {
-        var parrot_num = getRandomInt(0, 7);
-        var parrot_type; 
-        console.log(parrot_num);
-        switch(parrot_num) {
-            case 0: 
-                parrot_type = "party_parrot.gif";
-                break;
-            case 1: 
-                parrot_type = "shuffle_parrot.gif";
-                break;
-            case 2: 
-                parrot_type = "stable_parrot.gif";
-                break;
-            case 3: 
-                parrot_type = "mustache_parrot.gif";
-                break;
-            case 4: 
-                parrot_type = "fiesta_parrot.gif";
-                break;
-            case 5: 
-                parrot_type = "aussie_conga_parrot.gif";
-                break;
-            case 6: 
-                parrot_type = "triplets_parrot.gif";
-                break;
-        }
-        console.log(parrot_type)
-        return parrot_type;
-     }
     
     // vertical align image-preview text
     $(".event-img-container").css({'line-height': ($(".event-img-container").innerHeight() + 'px')});
@@ -55,3 +24,36 @@ function getRandomInt(min, max) {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+// party parrot ftw
+function party_parrot() {
+    var parrot_num = getRandomInt(0, 8);
+    var parrot_type; 
+    switch(parrot_num) {
+        case 0: 
+            parrot_type = "party_parrot.gif";
+            break;
+        case 1: 
+            parrot_type = "shuffle_parrot.gif";
+            break;
+        case 2: 
+            parrot_type = "stable_parrot.gif";
+            break;
+        case 3: 
+            parrot_type = "mustache_parrot.gif";
+            break;
+        case 4: 
+            parrot_type = "fiesta_parrot.gif";
+            break;
+        case 5: 
+            parrot_type = "aussie_conga_parrot.gif";
+            break;
+        case 6: 
+            parrot_type = "triplets_parrot.gif";
+            break;
+        case 7: 
+            parrot_type = "reverse_conga_parrot.gif";
+            break;
+    }
+    return parrot_type;
+ }
